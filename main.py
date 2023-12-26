d7e42c2c44c8f829d3a60bc71ec6eeeb8a233906
kalmanuel05 <151088021+kalmanuel05@users.noreply.github.com>
2023-12-26 00:15:38 -0500

    main.py

--- main.py
+++ main.py
@@ -1,49 +1,77 @@
-def list_todos(todos):
-    """
-        List todos
-    """
+def list_todos(to_dos):
 
-def add_todo(todos):
-    """
-        Add todos
-    """
+    print(to_dos)
 
-def edit_todo(todos):
-    """
-        Edit todos
-    """
+def add_todo(to_dos):
 
-def delete_todo(todos):
-    """
-        Edit todos
-    """
+  print("How many to do's would you like to add")
+  
+  counter = int(input("> "))
+  while counter <= 0: 
+    print("ERROR:(Please provide a valid input")
+    add_todo(to_dos)
 
-def menu(todos):
-    print("What would you like to do?")
-    print("1. List todos")
-    print("2. Add a todo")
-    print("3. Edit a todo")
-    print("4. Delete a todo")
-    print("5. Quit")
+  for i in range(counter): 
+     print("Enter to do's")
+     to_dos.append(input("> "))
+
+def edit_todo(to_dos):
+    
+    edit = to_dos.index(input("""What would you like to edit
+> """))
+    to_dos[edit] = input("""Enter edit
+> """)
+  
+def delete_todo(to_dos): 
+
+   print("Would you like to delete an item or would you like to delete all?")
+   
+   answer = input("> ")
+
+   if answer == "item": 
+    index = to_dos.index(input("""What item would you like to delete
+> """))
+    del to_dos[index]
+
+   elif answer == "all": 
+      del to_dos[:]
+   else: 
+      menu(to_dos)
+
+   print("""Would you like to delete more?""")
+   answer = input("> ")
+   if answer == "yes": 
+      delete_todo(to_dos)
+   else: 
+      menu(to_dos)
+
+def menu(to_dos):
+    print("""What would you like to do?
+1. List todos
+2. Add a todo
+3. Edit a todo
+4. Delete a todo
+5. Quit""")
     choice = input("> ")
     if choice == "1":
-      list_todos(todos)
+      list_todos(to_dos)
     elif choice == "2": 
-      add_todo(todos)
+      add_todo(to_dos)
     elif choice == "3": 
-      edit_todo(todos)
+      edit_todo(to_dos)
     elif choice == "4": 
-      delete_todo(todos)
+      delete_todo(to_dos)
     elif choice == "5": 
       print("Bye!")
       exit(0)
     else: 
       print("Ohh no! Invalid input.")
-    menu(todos)
+    menu(to_dos)
 
 def main():
+    
     print("Welcome to your todo list!")
-    todos = []
-    menu(todos)
+    to_dos = []
+    menu(to_dos)
 
 main()
